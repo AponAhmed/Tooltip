80a51cccdf7cdb494213b7b3c0fc5a2ebcd29ae0
MD. Muhiminul Haque <apon2041@gmail.com>
2024-01-03 16:42:50 +0600

    Updated - Remove DomBuilder dependency

--- src/Tooltip.js
+++ src/Tooltip.js
@@ -13,19 +13,22 @@ export default class Tooltip {
     }
 
     init() {
-        let el = new DOMBuilder();
-        this.tooltip = el.create('div').class('tooltip-container');
-        el = new DOMBuilder();
-        this.tooltipTitle = el.create('div').class('tooltip-title');
-        el = new DOMBuilder();
-        this.tooltipArrow = el.create('span').class('tooltip-arrow');
+        this.tooltip = document.createElement('div');
+        this.tooltip.classList.add('tooltip-container');
 
-        this.tooltip.append(this.tooltipArrow.element);
-        this.tooltip.append(this.tooltipTitle.element);
+        this.tooltipTitle = document.createElement('div');
+        this.tooltipTitle.classList.add('tooltip-title');
+
+        this.tooltipArrow = document.createElement('span');
+        this.tooltipArrow.classList.add('tooltip-arrow');
+
+        this.tooltip.appendChild(this.tooltipArrow);
+        this.tooltip.appendChild(this.tooltipTitle);
 
         this.item.addEventListener('mouseover', (e) => {
             this.showTooltip(this.item, e);
         });
+
         this.item.addEventListener('mouseout', () => {
             this.removeTooltip();
         });
@@ -46,39 +49,37 @@ export default class Tooltip {
             }
         }
 
-
-        this.tooltipArrow.element.removeAttribute('class');
-        this.tooltipArrow.element.removeAttribute('style');
-        this.tooltipArrow.class('tooltip-arrow');
-
+        this.tooltipArrow.removeAttribute('class');
+        this.tooltipArrow.removeAttribute('style');
+        this.tooltipArrow.classList.add('tooltip-arrow');
 
         if (this.position === 'right') {
-            this.tooltipArrow.element.style.borderRightColor = this.bg;
-            this.tooltip.element.style.left = this.exrect.left + this.exrect.width + this.extraMargin + 'px';
-            this.tooltipArrow.class('arrow-left');
+            this.tooltipArrow.style.borderRightColor = this.bg;
+            this.tooltip.style.left = this.exrect.left + this.exrect.width + this.extraMargin + 'px';
+            this.tooltipArrow.classList.add('arrow-left');
             let cntrT = (this.exrect.height - this.tooltipRect.height) / 2;
-            this.tooltip.element.style.top = this.exrect.top + cntrT + 'px';
+            this.tooltip.style.top = this.exrect.top + cntrT + 'px';
         } else if (this.position === 'left') {
-            this.tooltipArrow.element.style.borderLeftColor = this.bg;
-            this.tooltip.element.style.left = this.exrect.left - (this.tooltipRect.width + this.extraMargin) + 'px';
-            this.tooltipArrow.class('arrow-right');
+            this.tooltipArrow.style.borderLeftColor = this.bg;
+            this.tooltip.style.left = this.exrect.left - (this.tooltipRect.width + this.extraMargin) + 'px';
+            this.tooltipArrow.classList.add('arrow-right');
             let cntrT = (this.exrect.height - this.tooltipRect.height) / 2;
-            this.tooltip.element.style.top = this.exrect.top + cntrT + 'px';
+            this.tooltip.style.top = this.exrect.top + cntrT + 'px';
 
         } else if (this.position === 'top') {
-            this.tooltipArrow.element.style.borderTopColor = this.bg;
-            this.tooltipArrow.class('arrow-bottom');
+            this.tooltipArrow.style.borderTopColor = this.bg;
+            this.tooltipArrow.classList.add('arrow-bottom');
             let top = this.exrect.top - (this.exrect.height + this.extraMargin);
             let left = this.exrect.left + ((this.exrect.width - this.tooltipRect.width) / 2);
-            this.tooltip.element.style.top = top + 'px';
-            this.tooltip.element.style.left = left + 'px';
+            this.tooltip.style.top = top + 'px';
+            this.tooltip.style.left = left + 'px';
         } else {
-            this.tooltipArrow.element.style.borderBottomColor = this.bg;
-            this.tooltipArrow.class('arrow-top');
+            this.tooltipArrow.style.borderBottomColor = this.bg;
+            this.tooltipArrow.classList.add('arrow-top');
             let top = this.exrect.top + (this.exrect.height + this.extraMargin);
             let left = this.exrect.left + ((this.exrect.width - this.tooltipRect.width) / 2);
-            this.tooltip.element.style.top = top + 'px';
-            this.tooltip.element.style.left = left + 'px';
+            this.tooltip.style.top = top + 'px';
+            this.tooltip.style.left = left + 'px';
         }
     }
 
@@ -88,20 +89,19 @@ export default class Tooltip {
         } else {
             this.bg = '#1a1f30';
         }
-        this.tooltip.element.removeAttribute('style');
-
-        this.tooltip.element.style.backgroundColor = this.bg;
+        this.tooltip.removeAttribute('style');
+        this.tooltip.style.backgroundColor = this.bg;
     }
 
     showTooltip(item, e) {
         if (!this.text) {
             this.text = item.title;
         }
-        this.tooltipTitle.html(this.text);
+        this.tooltipTitle.innerHTML = this.text;
         this.exrect = item.getBoundingClientRect();
-        document.querySelector('body').appendChild(this.tooltip.element);
+        document.body.appendChild(this.tooltip);
         this.colorSet(item);
-        this.tooltipRect = this.tooltip.element.getBoundingClientRect();
+        this.tooltipRect = this.tooltip.getBoundingClientRect();
         this.positionSet(item);
     }
 
@@ -116,6 +116,6 @@ export default class Tooltip {
             this.item.addEventListener('mouseout', () => {
                 this.removeTooltip();
             });
-        })
+        });
     }
 }
